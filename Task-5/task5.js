4ed0da9043dd95996f89705b7c87511f3b5cf0ed
baikarrohit <baikarrohit1999@gmail.com>
2023-02-19 17:06:05 +0530

    task5 added

---
+++ Task-5/task5.js
@@ -0,0 +1,20 @@
+// ********** getElementsByClassName ************//
+
+// var item = document.getElementsByClassName('list-group-item');
+// 
+// item[2].style.backgroundColor = "green";
+// 
+// for(i=0; i<item.length; i++){
+//     item[i].style.fontWeight = "bold";
+// }
+
+// ********** getElementsByTagName ************//
+
+var li = document.getElementsByTagName('li');
+li[1].style.color = 'yellow';
+li[1].style.fontWeight = 'bold';
+
+for(i=0; i<li.length; i++){
+    li[i].style.backgroundColor = 'grey';
+}
+
